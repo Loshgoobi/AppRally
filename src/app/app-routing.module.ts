49e2fe96e9c39431c27068e5e5a1b3af4d76060f
Loshgoobi <remy.dozoul@ynov.com>
2019-05-30 11:00:01 +0200

    carte working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'planning', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'list', loadChildren: './list/list.module#ListPageModule' },
   { path: 'results', loadChildren: './pages/results/results.module#ResultsPageModule' },
--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-map',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
-  constructor() { }
+
+  constructor(private sanitize: DomSanitizer) {
+}
 
   ngOnInit() {
   }
